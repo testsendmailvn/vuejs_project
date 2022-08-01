be34fbb4cca5627f26edf86571abefeb8dcf9314
elearningvn <store.elearning.vn@gmail.com>
2022-08-02 06:32:33 +0700

    sdfs
    sdf

--- iPortfolio/src/Layouts/index.vue
+++ iPortfolio/src/Layouts/index.vue
@@ -1,12 +1,14 @@
   <!-- Khu vực import file -->
   <script setup>
         import TheHeader from '../Components/Headers/TheHeader.vue';
+        import TheWelcome from '../Components/Headers/TheWelcome.vue';
   </script>
 
   <!-- Khu vực hiển thị dữ liệu ra màn hình -->
   <template>
+    <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
     <TheHeader />
-
+    <TheWelcome />
     <main>
       
     </main>
